23e981a9527e365561c87b2b6d5eb947016ff234
freearduino <freearduino@gmail.com>
2022-06-12 12:00:45 +0300

    bugs time

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
+#define CLK 2
+#define DT 4
+#define SW 3
+#include "GyverEncoder.h"
+Encoder enc1(CLK, DT, SW);  // для работы c кнопкой
 #include <iarduino_OLED.h>  // Подключаем библиотеку iarduino_OLED.
 iarduino_OLED myOLED(0x3C); // Объявляем объект myOLED, указывая адрес дисплея на шине I2C: 0x3C или 0x3D.
-#include <EncButton.h>
 #include <iarduino_RTC.h>
-EncButton<EB_TICK, 3, 4, 2> enc; // энкодер с кнопкой <A, B, KEY>
-iarduino_RTC watch(RTC_DS1307);  // Объявляем объект watch для работы с RTC модулем на базе чипа DS1307, используется шина I2C.
-
-extern uint8_t MediumFont[]; // Подключаем шрифт MediumFont.
-uint32_t i;                  // Объявляем переменную для хранения времени прошедшего с момента старта скетча.
-int h = 0;                   // Объявляем переменную для хранения времени в часах, минутах и секундах.
+iarduino_RTC watch(RTC_DS1307); // Объявляем объект watch для работы с RTC модулем на базе чипа DS1307, используется шина I2C.
+extern uint8_t MediumFont[];    // Подключаем шрифт MediumFont.
+uint32_t i;                     // Объявляем переменную для хранения времени прошедшего с момента старта скетча.
+int h = 0;                      // Объявляем переменную для хранения времени в часах, минутах и секундах.
 int m = 0;
 int s = 0;
 int LedNumber = 0;
@@ -26,8 +28,28 @@ void setup()
     Serial.begin(9600);
     myOLED.begin();             // Инициируем работу с дисплеем.
     myOLED.setFont(MediumFont); // Указываем шрифт который требуется использовать для вывода цифр и текста.
-    enc.setEncType(EB_FULLSTEP);
+    enc1.setType(TYPE1);
 } //
+//Вывод часов
+void WriteH(int hour)
+{
+    myOLED.setCursor(16, 39); // Устанавливаем курсор в координату 16:31, это будет нижняя левая точка первой цифры выведенной функцией print().
+    if (hour < 10)
+    {
+        myOLED.print(0);
+    }
+    myOLED.print(hour);
+}
+void WriteM(int minut)
+{
+    myOLED.setCursor(51, 39); // Устанавливаем курсор в координату 16:31, это будет нижняя левая точка первой цифры выведенной функцией print().
+    if (minut < 10)
+    {
+        myOLED.print(0);
+    }
+    myOLED.print(minut);
+}
+//Вывод минут
 //п.п секунды
 void SecTimer()
 {
@@ -86,9 +108,9 @@ void TimeOnline()
 }
 void loop()
 {
-    enc.tick(); //Энкодер, опрос
+    enc1.tick();
     SecTimer();
-    if (enc.click()) //Если зажали энкодер
+    if (enc1.isClick()) //Если зажали энкодер
     {
 
         if (LedNumber < 2)
@@ -120,23 +142,26 @@ void loop()
         //Выводим надпись Settings Time
         if (FlagTimeSettings)
         {
+            enc1.tick();
             myOLED.clrScr();
             myOLED.setCursor(0, 20);
             myOLED.print("Time");
             FlagTimeSettings = false;
             FlagTime = true;
+            TimeOnline();
         }
         //Настройка времени
         while (!FlagTimeSettings)
         {
+            enc1.tick();
             //Настройка часов
             while (FlagHourTime)
             {
-                enc.tick(); //Энкодер, опрос
+                enc1.tick(); //Энкодер, опрос
                 //Поворот направо увеличение часов
-                if (enc.right())
+                if (enc1.isRight())
                 {
-                    Serial.println("right");
+                    // Serial.println("right");
                     if (h >= 23)
                     {
                         h = 0;
@@ -145,11 +170,12 @@ void loop()
                     {
                         h += 1;
                     }
+                    WriteH(h);
                 }
                 //Поворот налево уменьшение часов
-                if (enc.left())
+                if (enc1.isLeft())
                 {
-                    Serial.println("left");
+                    // Serial.println("left");
                     if (h <= 0)
                     {
                         h = 23;
@@ -158,13 +184,16 @@ void loop()
                     {
                         h -= 1;
                     }
+                    WriteH(h);
                 }
                 //Вывод часов
-                TimeOnline();
+
+                enc1.tick();
+                // TimeOnline();
                 //Если нажали выходим
-                if (enc.press())
+                if (enc1.isClick())
                 {
-                    Serial.println("Exit Minute");
+                    // Serial.println("Exit Hour");
                     FlagHourTime = false;
                 }
             }
@@ -172,40 +201,45 @@ void loop()
             //Настройка минут
             while (FlagMinutTime)
             {
-                enc.tick(); //Энкодер, опрос
+                enc1.tick(); //Энкодер, опрос
                 //Поворот направо увеличение часов
-                if (enc.right())
+                if (enc1.isRight())
                 {
-                    Serial.println("right");
+                    // Serial.println("right");
                     if (m >= 59)
                     {
-                        h = 0;
+                        m = 0;
                     }
                     else
                     {
-                        h += 1;
+                        m += 1;
                     }
+                    WriteM(m);
                 }
                 //Поворот налево уменьшение часов
-                if (enc.left())
+                if (enc1.isLeft())
                 {
-                    Serial.println("left");
-                    if (h <= 0)
+                    // Serial.println("left");
+                    if (m <= 0)
                     {
-                        h = 59;
+                        m = 59;
                     }
                     else
                     {
-                        h -= 1;
+                        m -= 1;
                     }
+                    WriteM(m);
                 }
                 //Вывод часов
-                TimeOnline();
+                // TimeOnline();
                 //Если нажали выходим
-                if (enc.press())
+                enc1.tick();
+                if (enc1.isClick())
                 {
-                    Serial.println("Exit hour");
+                    // Serial.println("Exit hour");
                     FlagMinutTime = false;
+                    FlagTimeSettings = true;
+                    FlagHourTime = true;
                 }
             }
 
@@ -229,12 +263,12 @@ void loop()
     }
 
     //Энкодер
-    if (enc.left())
-        Serial.println("left"); // поворот налево
-    if (enc.right())
-        Serial.println("right"); // поворот направо
-    if (enc.press())
-        Serial.println("press");
+    // if (enc1.isLeft())
+    //     Serial.println("left"); // поворот налево
+    // if (enc1.isRight())
+    //     Serial.println("right"); // поворот направо
+    // if (enc1.isClick())
+    //     Serial.println("press");
 
     // if (millis() % 1000 == 0)
     // {                                                     // Если прошла 1 секунда.
